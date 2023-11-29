980f0c8c84654f3e85a494f348bfd317c3bb18c1
Laptop Alnino <alnino007@gmail.com>
2023-11-29 17:35:42 +0700

    first commit

---
+++ lib/2_application/core/routes.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final routes = GoRouter(
+  initialLocation: '/home',
+  routes: [
+    GoRoute(
+      path: '/home/settings',
+      builder: (context, state) {
+        return Container(
+          color: Colors.amber,
+        );
+      },
+    ),
+    GoRoute(
+      path: '/home/start',
+      builder: (context, state) {
+        return Container(
+          color: Colors.blueGrey,
+        );
+      },
+    )
+  ],
+);
