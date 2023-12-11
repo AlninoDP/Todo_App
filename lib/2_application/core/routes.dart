cb4c61eb2d2f177d7d59634d6f7bcf46c0c6c2f8
Laptop Alnino <alnino007@gmail.com>
2023-12-11 10:38:22 +0700

    adding setting page + the routes

--- lib/2_application/core/routes.dart
+++ lib/2_application/core/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:todo_app/2_application/core/go_router_observer.dart';
 import 'package:todo_app/2_application/pages/home/home_page.dart';
+import 'package:todo_app/2_application/pages/settings/settings_page.dart';
 
 final GlobalKey<NavigatorState> _rootNavigationKey =
     GlobalKey<NavigatorState>(debugLabel: 'root');
@@ -10,34 +11,13 @@ final GlobalKey<NavigatorState> _shellNavigatorKey =
     GlobalKey<NavigatorState>(debugLabel: 'shell');
 
 final routes = GoRouter(
-  initialLocation: '/home',
+  initialLocation: '/home/dashboard',
   navigatorKey: _rootNavigationKey,
   observers: [GoRouterObserver()],
   routes: [
     GoRoute(
-      path: '/home/settings',
-      builder: (context, state) {
-        return Container(
-          color: Colors.amber,
-          child: Column(
-            children: [
-              ElevatedButton(
-                  onPressed: () => context.push('/home/start'),
-                  child: const Text('Start')),
-              TextButton(
-                  onPressed: () {
-                    if (context.canPop()) {
-                      context.pop();
-                    } else {
-                      context.push('/home/start');
-                    }
-                  },
-                  child: const Text('Back'))
-            ],
-          ),
-        );
-      },
-    ),
+        path: '/home/settings',
+        builder: (context, state) => const SettingsPage()),
 
     /// ShellRoute is used for:
     /// display routes in justa subsection of the screen
