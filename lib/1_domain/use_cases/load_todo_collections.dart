eb5fdcfbae2ae16b48c3edf638b0c1f9077f03bb
Laptop Alnino <alnino007@gmail.com>
2023-12-13 15:19:20 +0700

    making use cases + mock repository

---
+++ lib/1_domain/use_cases/load_todo_collections.dart
@@ -0,0 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:todo_app/1_domain/entities/todo_collection.dart';
+import 'package:todo_app/1_domain/failures/failures.dart';
+import 'package:todo_app/1_domain/repositories/todo_repository.dart';
+import 'package:todo_app/core/use_case.dart';
+
+class LoadToDoCollections implements UseCase<List<ToDoCollection>, NoParams> {
+  final ToDoRepository toDoRepository;
+  const LoadToDoCollections({required this.toDoRepository});
+
+  @override
+  Future<Either<Failure, List<ToDoCollection>>> call(NoParams params) async {
+    try {
+      final loadedCollections = await toDoRepository.readToDoCollections();
+      return loadedCollections.fold(
+        (left) => Left(left),
+        (right) => Right(right),
+      );
+    } on Exception catch (e) {
+      return Left(
+        ServerFailure(
+          stackTrace: e.toString(),
+        ),
+      );
+    }
+  }
+}
