96272b57dc7df6d236d836631e8ce435ef546623
Laptop Alnino <alnino007@gmail.com>
2023-12-17 14:14:31 +0700

    adding more method in repository to acces ToDoEntries

--- lib/0_data/repositories/todo_repository_mock.dart
+++ lib/0_data/repositories/todo_repository_mock.dart
@@ -1,25 +1,81 @@
 import 'package:dartz/dartz.dart';
 import 'package:todo_app/1_domain/entities/todo_collection.dart';
 import 'package:todo_app/1_domain/entities/todo_color.dart';
+import 'package:todo_app/1_domain/entities/todo_entry.dart';
 import 'package:todo_app/1_domain/entities/unique_id.dart';
 import 'package:todo_app/1_domain/failures/failures.dart';
 import 'package:todo_app/1_domain/repositories/todo_repository.dart';
 
 class ToDoRepositoryMock implements ToDoRepository {
+// List of todo entry
+  final List<ToDoEntry> toDoEntries = List.generate(
+    100,
+    (index) => ToDoEntry(
+        description: 'description $index',
+        id: EntryId.fromUniqueString(index.toString()),
+        isDone: false),
+  );
+
+// List Of TodoCollection
+  final toDoCollections = List<ToDoCollection>.generate(
+    10,
+    (index) => ToDoCollection(
+      id: CollectionId.fromUniqueString(index.toString()),
+      title: 'title $index',
+      color: ToDoColor(colorIndex: index % ToDoColor.predefinedColors.length),
+    ),
+  );
+
+// Method
   @override
   Future<Either<Failure, List<ToDoCollection>>> readToDoCollections() {
-    final list = List<ToDoCollection>.generate(
-      10,
-      (index) => ToDoCollection(
-        id: CollectionId.fromUniqueString(index.toString()),
-        title: 'title $index',
-        color: ToDoColor(colorIndex: index % ToDoColor.predefinedColors.length),
-      ),
-    );
+    try {
+      return Future.delayed(
+        const Duration(milliseconds: 200),
+        () => Right(toDoCollections),
+      );
+    } on Exception catch (e) {
+      return Future.value(Left(ServerFailure(stackTrace: e.toString())));
+    }
+  }
 
-    return Future.delayed(
-      const Duration(milliseconds: 200),
-      () => Right(list),
-    );
+  /// get the details of a specific to-do item
+  /// takes the ID of a specific to-do item (entryId)
+  /// looks through the list to find the item with that ID.
+  @override
+  Future<Either<Failure, ToDoEntry>> readToDoEntry(
+      CollectionId collectionId, EntryId entryId) {
+    try {
+      final selectedEntryItem =
+          toDoEntries.firstWhere((element) => element.id == entryId);
+
+      return Future.delayed(
+        const Duration(milliseconds: 200),
+        () => Right(selectedEntryItem),
+      );
+    } on Exception catch (e) {
+      return Future.value(Left(ServerFailure(stackTrace: e.toString())));
+    }
+  }
+
+  /// get a list of IDs for a group of tasks within a specific category.
+  /// takes the ID of a specific category (collectionId) and figures out a range of task IDs within that category.
+  /// returns a list of those task IDs
+  @override
+  Future<Either<Failure, List<EntryId>>> readToDoEntryIds(
+      CollectionId collectionId) {
+    try {
+      final startIndex = int.parse(collectionId.value) * 10;
+      final endIndex = startIndex + 10;
+      final entryIds = toDoEntries
+          .sublist(startIndex, endIndex)
+          .map((entry) => entry.id)
+          .toList();
+
+      return Future.delayed(
+          const Duration(milliseconds: 300), () => Right(entryIds));
+    } on Exception catch (e) {
+      return Future.value(Left(ServerFailure(stackTrace: e.toString())));
+    }
   }
 }
