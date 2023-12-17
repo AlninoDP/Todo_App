fa17af513ce980c69ac8b0c78d99f3baafeabf44
Laptop Alnino <alnino007@gmail.com>
2023-12-17 14:13:25 +0700

    adding ToDoEntryParam and CollectionIdParam to usecase

--- lib/core/use_case.dart
+++ lib/core/use_case.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:todo_app/1_domain/entities/unique_id.dart';
 import 'package:todo_app/1_domain/failures/failures.dart';
 
 abstract class UseCase<Type, Params> {
@@ -11,4 +12,24 @@ abstract class Params extends Equatable {}
 class NoParams extends Params {
   @override
   List<Object?> get props => [];
+}
+
+class ToDoEntryIdParam extends Params {
+  ToDoEntryIdParam({required this.collectionId, required this.entryId})
+      : super();
+
+  final EntryId entryId;
+  final CollectionId collectionId;
+
+  @override
+  List<Object?> get props => [collectionId, entryId];
+}
+
+class CollectionIdParam extends Params {
+  CollectionIdParam({required this.collectionId}) : super();
+
+  final CollectionId collectionId;
+
+  @override
+  List<Object?> get props => [collectionId];
 }
