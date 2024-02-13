b7d3d67c00d99afa6ce66f767159ec9bd79117f5
Laptop Alnino <alnino007@gmail.com>
2024-02-13 12:04:09 +0700

    update loading state in todo entry item

--- lib/2_application/components/todo_entry_item/view_states/todo_entry_item_loading.dart
+++ lib/2_application/components/todo_entry_item/view_states/todo_entry_item_loading.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class ToDoEntryItemLoading extends StatelessWidget {
   const ToDoEntryItemLoading({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const CircularProgressIndicator();
+    return Shimmer.fromColors(
+      baseColor: Colors.black38,
+      highlightColor: Colors.white,
+      child: const ListTile(
+        title: Text('Loading Data..'),
+        leading: CircularProgressIndicator(),
+      ),
+    );
   }
 }
