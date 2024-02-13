131080b3e8164928410638d2f6be00c6fc6dbfd0
Laptop Alnino <alnino007@gmail.com>
2024-02-13 10:43:21 +0700

    added equatable mixin into uniqe id class

--- lib/1_domain/entities/unique_id.dart
+++ lib/1_domain/entities/unique_id.dart
@@ -1,6 +1,7 @@
+import 'package:equatable/equatable.dart';
 import 'package:uuid/uuid.dart';
 
-class UniqueId {
+class UniqueId with EquatableMixin {
   final String value;
 
   // private constructor
@@ -13,6 +14,8 @@ class UniqueId {
   factory UniqueId.fromUniqueString(String uniqueString) {
     return UniqueId._(uniqueString);
   }
+  @override
+  List<Object?> get props => [value];
 }
 
 class CollectionId extends UniqueId {
