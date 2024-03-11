774abccd7037524b900d7a07a04f76325afd2203
Laptop Alnino <alnino007@gmail.com>
2024-03-11 16:28:22 +0700

    added create_todo_entry usecase in domain folder

---
+++ lib/1_domain/use_cases/create_todo_entry.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:todo_app/1_domain/failures/failures.dart';
+import 'package:todo_app/1_domain/repositories/todo_repository.dart';
+import 'package:todo_app/core/use_case.dart';
+
+class CreateToDoEntry implements UseCase<bool, ToDoEntryParams> {
+  final ToDoRepository toDoRepository;
+  CreateToDoEntry({required this.toDoRepository});
+
+  @override
+  Future<Either<Failure, bool>> call(params) async {
+    try {
+      final result = await toDoRepository.createToDoEntry(params.entry);
+      return result.fold(
+        (failure) => Left(failure),
+        (right) => Right(right),
+      );
+    } on Exception catch (e) {
+      return Left(ServerFailure(stackTrace: e.toString()));
+    }
+  }
+}
