ddbae36869682da142f44650fdc80506db46808e
Laptop Alnino <alnino007@gmail.com>
2024-03-13 10:52:47 +0700

    update better coma

--- lib/2_application/pages/create_todo_entry/bloc/create_todo_entry_page_cubit.dart
+++ lib/2_application/pages/create_todo_entry/bloc/create_todo_entry_page_cubit.dart
@@ -1,8 +1,10 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/1_domain/entities/todo_entry.dart';
 import 'package:todo_app/1_domain/entities/unique_id.dart';
 import 'package:todo_app/1_domain/use_cases/create_todo_entry.dart';
 import 'package:todo_app/2_application/core/form_value.dart';
+import 'package:todo_app/core/use_case.dart';
 
 part 'create_todo_entry_page_state.dart';
 
@@ -25,11 +27,17 @@ class CreateToDoEntryPageCubit extends Cubit<CreateToDoEntryPageState> {
 
     emit(
       state.copyWith(
-        description:
-            FormValue(value: description, validationStatus: currentStatus),
+        description: FormValue(
+          value: description,
+          validationStatus: currentStatus,
+        ),
       ),
     );
   }
 
-  void submit() {}
+  void submit() async {
+    await createToDoEntry.call(ToDoEntryParams(
+      entry: ToDoEntry.empty().copyWith(description: state.description?.value),
+    ));
+  }
 }
