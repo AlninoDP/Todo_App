69fde1f25263ded9867a7cccec7e5ad601494def
Laptop Alnino <alnino007@gmail.com>
2024-03-16 13:47:21 +0700

    update repository mock

--- lib/0_data/repositories/todo_repository_mock.dart
+++ lib/0_data/repositories/todo_repository_mock.dart
@@ -65,13 +65,16 @@ class ToDoRepositoryMock implements ToDoRepository {
     try {
       final startIndex = int.parse(collectionId.value) * 10;
       int endIndex = startIndex + 10;
-      if (toDoEntries.length <= endIndex) {
-        endIndex = toDoEntries.length - 1;
+      if (toDoEntries.length < endIndex) {
+        endIndex = toDoEntries.length;
       }
-      final entryIds = toDoEntries
-          .sublist(startIndex, endIndex)
-          .map((entry) => entry.id)
-          .toList();
+      List<EntryId> entryIds = [];
+      if (startIndex < toDoEntries.length) {
+        entryIds = toDoEntries
+            .sublist(startIndex, endIndex)
+            .map((entry) => entry.id)
+            .toList();
+      }
 
       return Future.delayed(
           const Duration(milliseconds: 300), () => Right(entryIds));
@@ -99,7 +102,11 @@ class ToDoRepositoryMock implements ToDoRepository {
   @override
   Future<Either<Failure, bool>> createToDoCollection(
       ToDoCollection collection) {
-    toDoCollections.add(collection);
+    final collectionToAdd = ToDoCollection(
+        id: CollectionId.fromUniqueString(toDoCollections.length.toString()),
+        title: collection.title,
+        color: collection.color);
+    toDoCollections.add(collectionToAdd);
     return Future.delayed(
         const Duration(milliseconds: 100), () => const Right(true));
   }
