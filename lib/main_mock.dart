12b1a0bfbbbe2399d5f2378c4abfa58dbc5c69dd
Laptop Alnino <alnino007@gmail.com>
2024-03-16 19:28:32 +0700

    update main to use local repo instead of mock

--- lib/main_mock.dart
+++ lib/main_mock.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:todo_app/0_data/data_sources/local/memory_local_data_source.dart';
-import 'package:todo_app/0_data/repositories/todo_local_repository.dart';
+import 'package:todo_app/0_data/repositories/todo_repository_mock.dart';
 import 'package:todo_app/1_domain/repositories/todo_repository.dart';
 import 'package:todo_app/2_application/app/basic_app.dart';
 
@@ -9,9 +8,7 @@ import 'package:todo_app/2_application/app/basic_app.dart';
 /// and is used to provide a repository to the widget tree.
 void main() {
   runApp(RepositoryProvider<ToDoRepository>(
-    create: (context) => ToDoLocalRepository(
-      localDataSource: MemoryLocalDataSource(),
-    ),
+    create: (context) => ToDoRepositoryMock(),
     child: const BasicApp(),
   ));
 }
